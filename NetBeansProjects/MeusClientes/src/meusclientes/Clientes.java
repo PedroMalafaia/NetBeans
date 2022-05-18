307b1ec5f64a0fab6d72aaa45561f0bf2de15f7c
PedroMalafaia <101152178+PedroMalafaia@users.noreply.github.com>
2022-05-17 21:03:58 -0300

    Add files via upload

---
+++ NetBeansProjects/MeusClientes/src/meusclientes/Clientes.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package meusclientes;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Clientes {
+    private int ID; 
+    private String NOME; 
+    private String TELEFONE; 
+    private String CELULAR; 
+    private String EMAIL; 
+    
+    public Clientes(int id, String nome, String telefone, String celular, String email) { 
+        this.ID = id; 
+        this.NOME = nome; 
+        this.TELEFONE = telefone; 
+        this.CELULAR = celular; 
+        this.EMAIL = email; 
+    } 
+    public Clientes(){ 
+    } 
+    public Clientes(String nome, String telefone, String celular, String email) { this.NOME = nome; 
+        this.TELEFONE = telefone; 
+        this.CELULAR = celular; 
+        this.EMAIL = email; 
+    } 
+    public String getCELULAR() { 
+        return CELULAR; 
+    } 
+    public void setCELULAR(String CELULAR) { 
+        this.CELULAR = CELULAR; 
+    } 
+    public String getEMAIL() { 
+        return EMAIL; 
+    } 
+    public void setEMAIL(String EMAIL) { 
+        this.EMAIL = EMAIL; 
+    } 
+    public int getID() { 
+        return ID; 
+    } 
+    public void setID(int ID) { 
+        this.ID = ID; 
+    } 
+    public String getNOME() { 
+     return NOME; 
+    } 
+    public void setNOME(String NOME) { 
+    this.NOME = NOME; 
+    } 
+    public String getTELEFONE() { 
+    return TELEFONE; 
+    } 
+    public void setTELEFONE(String TELEFONE) { 
+    this.TELEFONE = TELEFONE; 
+    } 
+} //fim da class clientes
---
+++ NetBeansProjects/MeusClientes/src/meusclientes/MeusClientes.java
@@ -0,0 +1,13 @@
+package meusclientes;
+
+import telas.menu;
+import telas.*;
+
+public class MeusClientes {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        menu MENU = new menu();
+        MENU.setVisible(true);
+       }
+}
